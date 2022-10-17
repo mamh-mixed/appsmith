85f35bfc47167edac5dff5cf0d2013e3af666051
Rimil Dey <rimil@appsmith.com>
2022-10-17 16:21:57 +0530

    Remove feature flags Remove unnecessary code for baseoptions Add js function to base options

--- app/client/src/components/editorComponents/ActionCreator/index.tsx
+++ app/client/src/components/editorComponents/ActionCreator/index.tsx
@@ -54,8 +54,6 @@ import {
 import { setGlobalSearchCategory } from "actions/globalSearchActions";
 import { filterCategories, SEARCH_CATEGORY_ID } from "../GlobalSearch/utils";
 import { ActionDataState } from "reducers/entityReducers/actionsReducer";
-import { selectFeatureFlags } from "selectors/usersSelectors";
-import FeatureFlags from "entities/FeatureFlags";
 import { connect } from "react-redux";
 import { isValidURL } from "utils/URLUtils";
 import { ACTION_ANONYMOUS_FUNC_REGEX, ACTION_TRIGGER_REGEX } from "./regex";
@@ -68,7 +66,11 @@ import {
 import { SwitchType, ActionCreatorProps, GenericFunction } from "./types";
 import { FIELD_GROUP_CONFIG } from "./FieldGroup/FieldGroupConfig";
 
-const baseOptions: { label: string; value: string }[] = [
+const baseOptions: {
+  label: string;
+  value: string;
+  children?: TreeDropdownOption[];
+}[] = [
   {
     label: createMessage(NO_ACTION),
     value: AppsmithFunction.none,
@@ -76,6 +78,12 @@ const baseOptions: { label: string; value: string }[] = [
   {
     label: createMessage(EXECUTE_A_QUERY),
     value: AppsmithFunction.integration,
+    children: [],
+  },
+  {
+    label: createMessage(EXECUTE_JS_FUNCTION),
+    value: AppsmithFunction.jsFunction,
+    children: [],
   },
   {
     label: createMessage(NAVIGATE_TO),
@@ -134,22 +142,6 @@ const baseOptions: { label: string; value: string }[] = [
     value: AppsmithFunction.postMessage,
   },
 ];
-
-const getBaseOptions = (featureFlags: FeatureFlags) => {
-  const { JS_EDITOR: isJSEditorEnabled } = featureFlags;
-  if (isJSEditorEnabled) {
-    const jsOption = baseOptions.find(
-      (option: any) => option.value === AppsmithFunction.jsFunction,
-    );
-    if (!jsOption) {
-      baseOptions.splice(2, 0, {
-        label: createMessage(EXECUTE_JS_FUNCTION),
-        value: AppsmithFunction.jsFunction,
-      });
-    }
-  }
-  return baseOptions;
-};
 
 function getFieldFromValue(
   value: string | undefined,
@@ -361,14 +353,11 @@ function getIntegrationOptionsWithChildren(
   pageId: string,
   applicationId: string,
   plugins: any,
-  options: TreeDropdownOption[],
   actions: ActionDataState,
   jsActions: Array<JSCollectionData>,
   createIntegrationOption: TreeDropdownOption,
   dispatch: any,
-  featureFlags: FeatureFlags,
 ) {
-  const { JS_EDITOR: isJSEditorEnabled } = featureFlags;
   const createJSObject: TreeDropdownOption = {
     label: "New JS Object",
     value: "JSObject",
@@ -388,11 +377,11 @@ function getIntegrationOptionsWithChildren(
       action.config.pluginType === PluginType.SAAS ||
       action.config.pluginType === PluginType.REMOTE,
   );
-  const option = options.find(
+  const option = baseOptions.find(
     (option) => option.value === AppsmithFunction.integration,
   );
 
-  const jsOption = options.find(
+  const jsOption = baseOptions.find(
     (option) => option.value === AppsmithFunction.jsFunction,
   );
 
@@ -424,7 +413,7 @@ function getIntegrationOptionsWithChildren(
       } as TreeDropdownOption);
     });
   }
-  if (isJSEditorEnabled && jsOption) {
+  if (jsOption) {
     jsOption.children = [createJSObject];
     jsActions.forEach((jsAction) => {
       if (jsAction.config.actions && jsAction.config.actions.length > 0) {
@@ -475,13 +464,12 @@ function getIntegrationOptionsWithChildren(
       }
     });
   }
-  return options;
+  return baseOptions;
 }
 
 function useIntegrationsOptionTree() {
   const pageId = useSelector(getCurrentPageId) || "";
   const applicationId = useSelector(getCurrentApplicationId) as string;
-  const featureFlags = useSelector(selectFeatureFlags);
   const dispatch = useDispatch();
   const plugins = useSelector((state: AppState) => {
     return state.entities.plugins.list;
@@ -494,7 +482,6 @@ function useIntegrationsOptionTree() {
     pageId,
     applicationId,
     pluginGroups,
-    getBaseOptions(featureFlags),
     actions,
     jsActions,
     {
@@ -512,7 +499,6 @@ function useIntegrationsOptionTree() {
       },
     },
     dispatch,
-    featureFlags,
   );
 }
 
@@ -554,6 +540,7 @@ const ActionCreator = React.forwardRef(
     );
     const dataTree = useSelector(getDataTree);
     const integrationOptionTree = useIntegrationsOptionTree();
+    console.log(integrationOptionTree);
     const widgetOptionTree = useSelector(getWidgetOptionsTree);
     const modalDropdownList = useModalDropdownList();
     const fields = getFieldFromValue(
